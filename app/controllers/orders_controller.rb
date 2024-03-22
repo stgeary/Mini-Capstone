c64fab052708fcff911e78541a3a1e642f0d9255
stgeary <scottmgeary17@gmail.com>
2024-03-22 12:29:36 -0500

    Made create order method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,18 @@
 class OrdersController < ApplicationController
+    def create
+        @order = Order.create(
+            user_id: params["user_id"],
+            product_id: params["product_id"],
+            quantity: params["quantity"],
+            subtotal: params["subtotal"],
+            tax: params["tax"],
+            total: params["total"]
+        )
+        if @order.valid?
+            render :show
+        else
+            render json: { errors: @order.errors.full_messages},
+            status: 22
+        end
+    end
 end
