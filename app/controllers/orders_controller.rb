4f46f028615e6d6db182da194ea448059fb955c5
stgeary <scottmgeary17@gmail.com>
2024-03-22 12:57:12 -0500

    Added show method to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,10 @@
 class OrdersController < ApplicationController
 
+    def show
+        @order = Order.find_by(id: params[:id])
+        render :show
+    end
+
     def index
         orders = Order.all.order(:id)
         render json: orders
--- config/routes.rb
+++ config/routes.rb
@@ -36,5 +36,6 @@ Rails.application.routes.draw do
   post "/sessions" => "sessions#create"
 
   get "/orders" => "orders#index"
+  get "/orders/:id" => "orders#show"
   post "/orders" => "orders#create"
 end
