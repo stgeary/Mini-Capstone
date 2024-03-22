fb9d802ab956db0fc93ece369b0b4c9adcb1368a
stgeary <scottmgeary17@gmail.com>
2024-03-22 13:58:07 -0500

    got sessions working

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,9 +19,9 @@ class OrdersController < ApplicationController
             user_id: params["user_id"],
             product_id: params["product_id"],
             quantity: params["quantity"],
-            subtotal: params["subtotal"],
-            tax: params["tax"],
-            total: params["total"]
+            # subtotal: params["subtotal"],
+            # tax: params["tax"],
+            # total: params["total"]
         )
         if @order.valid?
             render :show
