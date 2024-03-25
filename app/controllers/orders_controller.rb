3bf99f9174d758608e68704d3eb0d4db5bb4af2e
stgeary <scottmgeary17@gmail.com>
2024-03-25 14:11:05 -0500

    Added authentiaction

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,16 +5,29 @@ class OrdersController < ApplicationController
     end
 
     def index
-        orders = Order.all.order(:id)
-        render json: orders
-    end
+        if current_user
+          @orders = current_user.orders
+          render :index
+        else
+          render json: [], status: :unauthorized
+        end
+      end
 
     def create
+        product = Product.find_by(id: params["product_id"])
+
+        calculated_subtotal = product.price * params["quantity"].to_i
+        calculated_tax = calculated_subtotal / 10
+        calculated_total = calculated_subtotal + calculated_tax
+
         @order = current_user.orders.new(
             user_id: params[:user_id],
             product_id: params[:product_id],
             quantity: params[:quantity],
             subtotal: params[:subtotal],
+            subtotal: calculated_subtotal,
+            tax: calculated_tax,
+            total: calculated_total,
         )
         if @order.save
             render :show
